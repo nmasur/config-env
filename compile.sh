7a9c96f884c6fb003fbf35e517cd50f2b3e96472
Noah Masur <noah.masur@take2games.com>
2020-07-15 17:17:57 -0400

    Test versioning of releases

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cargo build \
-      --release \
-      --target x86_64-unknown-linux-musl \
-    && cargo pkgid \
-        | cut -d# -f2 \
-        | cut -d: -f2 \
-              > ./target/x86_64-unknown-linux-musl/release/VERSION
--- example.sh
+++ example.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Download static binary and unzip
 curl -L https://github.com/nmasur/config-env/releases/download/latest/config-env.zip --output config-env.zip
 unzip config-env.zip
 rm config-env.zip
+
+# Build docker image (copies static binary)
 docker build -t config-env-example -f example.Dockerfile ./
+
+# Run docker container, adding environment variables if not already available
+# AWS creds can be provided from credentials file, env variables, or IAM role from EC2 or other system
 docker run --rm -e APPLICATION=pd-store -e ENVIRONMENT=production -e VERSION=blue -v $HOME/.aws/credentials:/root/.aws/credentials config-env-example
